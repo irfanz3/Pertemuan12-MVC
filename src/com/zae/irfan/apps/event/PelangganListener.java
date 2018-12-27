f1b11da43f04b34f811677d97e443a8e38bbd5e4
irfanz3 <irfan.zaelani13@gmail.com>
2018-12-28 01:14:22 +0700

    irfan 10117911

---
+++ src/com/zae/irfan/apps/event/PelangganListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zae.irfan.apps.event;
+
+import com.zae.irfan.apps.model.PelangganModel;
+
+/**
+ *
+ * @author Irfan Zaelani 10117911
+ */
+public interface PelangganListener {
+    public void onChange(PelangganModel pelanggan);
+}
---
+++ src/com/zae/irfan/apps/model/LatihanMVC.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zae.irfan.apps.model;
+
+import com.zae.irfan.apps.view.PelangganView;
+
+/**
+ *
+ * @author Irfan Zaelani 10117911
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PelangganView view = new PelangganView();
+        view.setVisible(true);
+    }
+
+}
